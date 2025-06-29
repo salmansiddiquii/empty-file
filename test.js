57703dff95c285e17855bea0c06a845aa4fa41a9
Salman <siddiquisalman@gmail.com>
2025-06-29 08:18:12 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,21 @@
+// // test.js
+// "use strict";
+// const { calC } = require("./calculator.js");
+
+const calC = (num1, num2, operator) => {
+  switch (operator) {
+    case "+":
+      console.log(num1 + num2);
+    case "-":
+      console.log(num1 - num2);
+    case "*":
+      console.log(num1 * num2);
+    case "/":
+      if (num2 === 0) {
+        throw new Error("Division by zero is not allowed");
+      }
+      console.log(num1 / num2);
+    default:
+      throw new Error("Invalid operator");
+  }
+};
